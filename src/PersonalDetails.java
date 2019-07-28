f69c3aa0e5a4f56620e8adfb98c97aa0db16fffb
Disha <dishadhanki1998@gmail.com>
2019-07-28 21:08:53 +0100

    "Personal Details Java Intro"

---
+++ src/PersonalDetails.java
@@ -0,0 +1,17 @@
+public class PersonalDetails
+{
+
+    public static void main(String[] args)
+    {
+        String name = "Disha";
+        String fulladdress = "18 Filbert Village, Filbert Street Leicestershire UK LE2 7AF";
+        Long mobile = 7898482932L;
+        Float salary = 1500.85F;
+
+        System.out.println(name);
+        System.out.println(fulladdress);
+        System.out.println(mobile);
+        System.out.println(salary);
+
+    }
+}
